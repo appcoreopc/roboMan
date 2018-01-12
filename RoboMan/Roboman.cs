d7b06ab44a5d589affd2f96ad8619510cd379f40
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-01-12 16:38:18 +1300

    implemented interface.

--- RoboMan/Roboman.cs
+++ RoboMan/Roboman.cs
@@ -4,10 +4,36 @@ using System.Text;
 
 namespace RoboMan
 {
-    class Roboman  : IRobo
+    class Roboman : IRobo
     {
+        public bool Left()
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool Move()
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool Place(int x, int y, FaceDirection facingDirection)
+        {
+            throw new NotImplementedException();
+        }
 
+        public string ReportStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Right()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Turn(Direction direction)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
