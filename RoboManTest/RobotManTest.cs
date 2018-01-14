f691bf8cfd511d8901e42b6d0c9f0a293c17bb83
kepung <kepung@gmail.com>
2018-01-14 21:38:44 +1300

    added more test scenarios

--- RoboManTest/RobotManTest.cs
+++ RoboManTest/RobotManTest.cs
@@ -16,7 +16,7 @@ namespace RoboManTest
             _board = new Simple5x5Board(Appconstant.DefaultBoardSize);
             _roboman = new Roboman(_board);
         }
-        
+
         [TestMethod]
         public void PlaceRobotWithinBoardTest()
         {
@@ -27,12 +27,91 @@ namespace RoboManTest
         }
 
         [TestMethod]
-        public void PlaceRobotOutOfBoardSize()
+        public void PlaceRobotOutOfBoardSizeTest()
         {
             var result = _roboman.SetPositionOnBoard(10, 10, FaceDirection.North);
             Assert.IsNotNull(result);
             Assert.IsTrue(result.Status == MovementStatus.MoveXOutOfBoardMaxSize || result.Status == MovementStatus.MoveYOutOfBoardMaxSize);
         }
+
+        [TestMethod]
+        public void MoveWithinYAxiValidTest()
+        {
+            var result = _roboman.SetPositionOnBoard(0, 3, FaceDirection.North);
+            var validMovement = _roboman.Move();
+            Assert.IsTrue(validMovement.Status == MovementStatus.MoveOk);
+
+        }
+
+        [TestMethod]
+        public void MoveOutsideYAxiTest()
+        {
+            var result = _roboman.SetPositionOnBoard(0, 3, FaceDirection.North);
+
+            _roboman.Move();
+
+            var invalidMovement = _roboman.Move();
+
+            Assert.IsTrue(invalidMovement.Status == MovementStatus.UnableToMoveToTargetLocation);
+        }
+
+        
+        [TestMethod]
+        public void LeftTurnDirectionValidTest()
+        {
+            var result = _roboman.SetPositionOnBoard(0, 3, FaceDirection.North);
+
+            _roboman.Left();
+
+            var currentDirection = _roboman.ReportStatus();
+
+            Assert.IsTrue(currentDirection.Direction == FaceDirection.West);
+        }
+
+        [TestMethod]
+        public void LeftTurnSouthValidTest()
+        {
+            var result = _roboman.SetPositionOnBoard(0, 3, FaceDirection.North);
+
+            _roboman.Left();
+
+            _roboman.Left();
+
+            var currentDirection = _roboman.ReportStatus();
+
+            Assert.IsTrue(currentDirection.Direction == FaceDirection.South);
+        }
+
+
+
+        [TestMethod]
+        public void RightTurnDirectionValidTest()
+        {
+            var result = _roboman.SetPositionOnBoard(0, 3, FaceDirection.North);
+
+            _roboman.Right();
+
+            var currentDirection = _roboman.ReportStatus();
+
+            Assert.IsTrue(currentDirection.Direction == FaceDirection.East);
+        }
+
+
+        [TestMethod]
+        public void ThreeSixtyTurnDirectionValidTest()
+        {
+            var result = _roboman.SetPositionOnBoard(0, 3, FaceDirection.North);
+
+            _roboman.Right();
+            _roboman.Right();
+            _roboman.Right();
+            _roboman.Right();
+
+            var currentDirection = _roboman.ReportStatus();
+
+            Assert.IsTrue(currentDirection.Direction == FaceDirection.North);
+        }
+
 
 
     }
