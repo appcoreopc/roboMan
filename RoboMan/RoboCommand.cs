bb0374c27aaa3e34ccfc68af8f5f55b2a7a936a9
kepung <kepung@gmail.com>
2018-01-14 18:01:37 +1300

    implement command parsing and action for robot.

--- RoboMan/RoboCommand.cs
+++ RoboMan/RoboCommand.cs
@@ -1,13 +1,26 @@
-﻿using System;
-using CommandLine;
+﻿using CommandLine;
 
 namespace RoboMan
 {
-
     [Verb("place", HelpText = "Place robot on a given board")]
     class PlaceOptions
     {
-        //normal options here
+        // Alternative implementation /
+        //[Value(0)]
+        //public int LocationX { get; set; }
+
+        //[Value(1)]
+        //public int LocationY { get; set; }
+
+        //[Value(2)]
+        //public FaceDirection direction { get; set; }
+
+        //[Value(0, Min = 1, Max = 1)]
+        //public IEnumerable<string> PlacementCommandArgument { get; set; }
+
+        [Value(0)]
+        public string PlacementCommandArgument { get; set; }
+
     }
 
     [Verb("move", HelpText = "Move forward in the facing direction")]
@@ -46,39 +59,53 @@ namespace RoboMan
 
         public void ExecuteCommand(string[] command)
         {
+
             var x = CommandLine.Parser.Default.ParseArguments<PlaceOptions, MoveOptions, LeftOptions, RightOptions, ReportOptions>(command)
            .MapResult(
-           (PlaceOptions opts) => PlaceRobot(opts),
-           (MoveOptions opts) => RunAddAndReturnExitCode(opts),
-           (LeftOptions opts) => RunCommitAndReturnExitCode(opts),
-           (RightOptions opts) => RunCloneAndReturnExitCode(opts),
-           (ReportOptions opts) => RunReportOption(opts),
-           errs => 1);
+
+                (PlaceOptions opts) => PlaceRobot(opts),
+
+                (MoveOptions opts) => Move(opts),
+
+                (LeftOptions opts) => TurnLeft(opts),
+
+                (RightOptions opts) => TurnRight(opts),
+
+                (ReportOptions opts) => ReportStatus(opts),
+
+                errs => 1);
         }
 
         private object PlaceRobot(PlaceOptions opts)
         {
-            throw new NotImplementedException();
+            if (GetPlacementDirection(opts.PlacementCommandArgument))
+                return true;
+            return false;
         }
 
-        private object RunReportOption(ReportOptions opts)
+        private bool GetPlacementDirection(string placementCommandArgument)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
-        private object RunCloneAndReturnExitCode(RightOptions opts)
+        private object ReportStatus(ReportOptions opts)
         {
-            throw new NotImplementedException();
+            return _robo.ReportStatus();
         }
 
-        private object RunCommitAndReturnExitCode(LeftOptions opts)
+        private object TurnRight(RightOptions opts)
         {
-            return 1;
+            return _robo.Right();
         }
 
-        private object RunAddAndReturnExitCode(MoveOptions opts)
+        private object TurnLeft(LeftOptions opts)
         {
-            return 0;
+            return _robo.Left();
+        }
+
+        private object Move(MoveOptions opts)
+        {
+            return _robo.Move();
         }
     }
 }
