be5a9f9856e1f0f2caa81f676865442c9f694137
kepung <kepung@gmail.com>
2018-01-14 22:36:09 +1300

    implemented command result decoder

--- RoboMan/Appconstant.cs
+++ RoboMan/Appconstant.cs
@@ -20,8 +20,37 @@ namespace RoboMan
 
         public const string RobotPlaceInvalidCommandParsed = "Command is not parsed correctly.";
 
-        public const string TurnLeftSuccessful = "Turn left ok";
+        public const string TurnLeftSuccessful = "Turn left ok.";
 
+        public const string TurnLeftUnable = "Unable Turn left.";
+
+        public const string TurnRightSuccessful = "Turn right ok";
+
+        public const string TurnRightFail = "Unable Turn right.";
+
+        public const string MoveOk = "Move Ok";
+
+        public const string MoveXOutOfBoardMaxSize = "Moving on x-axis out of board.";
+
+        public const string LeftTurnFail = "Turn left failed.";
+
+        public const string RightTurnOk = "Right turn ok.";
+
+        public const string RightTurnFail = "Right turn failed";
+
+        public const string ChangeDirectionOk = "Change direction ok";
+
+        public const string ChangeDirectionFailed = "Change direction failed.";
+
+        public const string MoveXOutOfBoardMinSize = "Move x-axis is negative";
+
+        public const string UnableToMoveToTargetLocation = "Unable to move out of board";
+
+        public const string MoveYOutOfBoardMaxSize = "Moving y-axis out of board size.";
+
+        public const string MoveYOutOfBoardMinSize = "Move y-axis is negative.";
+
+        public const string MoveCannotBeDetermined = "Move cannot be determined.";
     }
 
 
--- RoboMan/Command/CommandResultDecoder.cs
+++ RoboMan/Command/CommandResultDecoder.cs
@@ -24,36 +24,51 @@ namespace RoboMan.Command
                     System.Console.WriteLine(Appconstant.TurnLeftSuccessful);
                     break;
                 case MovementStatus.TurnLeftUnable:
+                    System.Console.WriteLine(Appconstant.TurnLeftUnable);
                     break;
                 case MovementStatus.TurnRightSuccessful:
+                    System.Console.WriteLine(Appconstant.TurnRightSuccessful);
                     break;
                 case MovementStatus.TurnRightFail:
+                    System.Console.WriteLine(Appconstant.TurnRightFail);
                     break;
                 case MovementStatus.MoveOk:
+                    System.Console.WriteLine(Appconstant.MoveOk);
                     break;
                 case MovementStatus.MoveXOutOfBoardMaxSize:
+                    System.Console.WriteLine(Appconstant.MoveXOutOfBoardMaxSize);
                     break;
                 case MovementStatus.MoveXOutOfBoardMinSize:
+                    System.Console.WriteLine(Appconstant.MoveXOutOfBoardMinSize);
                     break;
                 case MovementStatus.UnableToMoveToTargetLocation:
+                    System.Console.WriteLine(Appconstant.UnableToMoveToTargetLocation);
                     break;
                 case MovementStatus.MoveYOutOfBoardMaxSize:
+                    System.Console.WriteLine(Appconstant.MoveYOutOfBoardMaxSize);
                     break;
                 case MovementStatus.MoveYOutOfBoardMinSize:
+                    System.Console.WriteLine(Appconstant.MoveYOutOfBoardMinSize);
                     break;
                 case MovementStatus.MoveCannotBeDetermined:
+                    System.Console.WriteLine(Appconstant.MoveCannotBeDetermined);
                     break;
                 case MovementStatus.LeftTurnOk:
                     break;
                 case MovementStatus.LeftTurnFail:
+                    System.Console.WriteLine(Appconstant.LeftTurnFail);
                     break;
                 case MovementStatus.RightTurnOk:
+                    System.Console.WriteLine(Appconstant.RightTurnOk);
                     break;
                 case MovementStatus.RightTurnFail:
+                    System.Console.WriteLine(Appconstant.RightTurnFail);
                     break;
                 case MovementStatus.ChangeDirectionOk:
+                    System.Console.WriteLine(Appconstant.ChangeDirectionOk);
                     break;
                 case MovementStatus.ChangeDirectionFailed:
+                    System.Console.WriteLine(Appconstant.ChangeDirectionFailed);
                     break;
                 default:
                     break;
